1131d1e60042550470438b7311b10beb93e62fa0
Dorin Matei <matei.dorin22@gmail.com>
2023-02-16 07:39:46 +0200

    hangman first commit

---
+++ hangman/main.py
@@ -0,0 +1,23 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+#     getting user input
+    user_letter = input('Guess a letter:  ').upper()
+
+user_input = input('Type something:  ')
+
+print(user_input)
